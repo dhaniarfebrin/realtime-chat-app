e26c50d07e51eab91c916fd0154b86bc160b96d6
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-07-25 00:18:16 +0700

    chore: add routing frontend

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("form");
+    };
+    const handleChange = (e) => {};
+    return (
+        <>
+            <FormContainer>
+                <form onSubmit={(event) => handleSubmit(event)}>
+                    <div className="brand">
+                        <img src={Logo} alt="" />
+                        <h1>snappy</h1>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="confirmPassword"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <button type="submit">Create Account</button>
+                    <span>
+                        already have an account? <Link to="/login">Login</Link>
+                    </span>
+                </form>
+            </FormContainer>
+        </>
+    );
 }
 
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .brand {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img {
+            height: 5rem;
+        }
+    }
+`;
